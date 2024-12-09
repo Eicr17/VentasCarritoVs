c1ab93f6e37784bcea956d6864e65e3f6d0aa293
Raúl Canel <rcanel@agexport.org.gt>
2024-12-09 15:36:54 -0600

    Se agregan cors

--- VentasCarrito/VentasCarrito/Modelos/ProductoApi.cs
+++ VentasCarrito/VentasCarrito/Modelos/ProductoApi.cs
@@ -2,8 +2,8 @@
 {
     public class ProductoApi
     {
-        public int Id_Producto { get; set; }
-        public string Nombre_Producto { get; set; }
+        public int id_producto { get; set; }
+        public string nombre_producto { get; set; }
         public int Existencia { get; set; }
         public string Marca { get; set; }
         public decimal Precio { get; set; }
